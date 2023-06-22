971a34fb630c56dc55a45a98bdb862649643812a
a-isaiahharvey <41553768+a-isaiahharvey@users.noreply.github.com>
2023-06-22 11:40:15 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,14 +6,14 @@ import PackageDescription
 let package = Package(
   name: "pong-sdl",
   dependencies: [
-    .package(url: "https://github.com/PureSwift/SDL", branch: "master")
+    .package(url: "https://github.com/a-isaiahharvey/SwiftSDL2.git", branch: "main")
   ],
   targets: [
     // Targets are the basic building blocks of a package, defining a module or a test suite.
     // Targets can depend on other targets in this package and products from dependencies.
     .executableTarget(
       name: "pong-sdl",
-      dependencies: ["SDL"],
+      dependencies: ["SwiftSDL2"],
       path: "Sources")
   ]
 )
